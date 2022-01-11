f0e2567bd992b0c644bc90d5e020d8b2294eaa41
Nuno-Ferreira <nc766@live.mdx.ac.uk>
2022-01-11 16:02:08 +0000

    adding first animation

--- basic animations week12.py
+++ basic animations week12.py
@@ -8,6 +8,9 @@ client = opc.Client('localhost:7890')
 client.put_pixels(leds)
 client.put_pixels(leds)
 
-
+for led in range(len(leds)):
+    leds[led] = (255,0,0)
+    time.sleep(1)
+    client.put_pixels(leds)
 
 
