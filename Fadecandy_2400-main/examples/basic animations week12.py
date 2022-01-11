ad125ef0b1f0556ccf751d93a2af8129c6ead92b
Nuno-Ferreira <nc766@live.mdx.ac.uk>
2022-01-11 19:06:46 +0000

    added animations

--- Fadecandy_2400-main/examples/basic animations week12.py
+++ Fadecandy_2400-main/examples/basic animations week12.py
@@ -15,22 +15,29 @@ for led in range(60): #picks outs the leds
 
 led = 0 #to reverse it change to 59
 while led<60: #to reverse chaneg it to 'led>=0'
-    leds[led] = (0,255,0) # green
+    leds[led] = (0,255,0) # green/[59-led] to reverse easily
     time.sleep(.1)
     client.put_pixels(leds)
     led = led + 1 #'-1' to reverse the order
 
 led = 0
-while led<60: #scroll all rows at the same time
-    leds[led] = (0,0,255) #row 1 - 0-60
-    leds[led+60] = (0,0,255) #row 2 - 0+60 - 59+60 (60-119)
-    leds[led+120] = (0,0,255)
-    leds[led+180] = (0,0,255) #this can be done in 2 lines instead of 6
-    leds[led+240] = (0,0,255)
-    leds[led+300] = (0,0,255)
+while led<60:
+    for rows in range(6):
+        leds[led + rows*60] = (0, 0, 255)
     client.put_pixels(leds)
     time.sleep(.1)
     led = led + 1
+                 
+#while led<60: #scroll all rows at the same time
+    #leds[led] = (0,0,255) #row 1 - 0-60
+    #leds[led+60] = (0,0,255) #row 2 - 0+60 - 59+60 (60-119)
+    #leds[led+120] = (0,0,255)
+    #leds[led+180] = (0,0,255) #this can be done in 2 lines instead of 6
+    #leds[led+240] = (0,0,255)
+    #leds[led+300] = (0,0,255)
+    #client.put_pixels(leds)
+    #time.sleep(.1)
+    #led = led + 1
 
 # refactor the last example to use less code
 # reverse the last example.
