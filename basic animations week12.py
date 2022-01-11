f75450e0fb63386439dbb324d70f57b65417fe2b
Nuno-Ferreira <nc766@live.mdx.ac.uk>
2022-01-11 15:59:43 +0000

    basic structure

--- basic animations week12.py
+++ basic animations week12.py
@@ -1,1 +1,13 @@
 import opc
+import time
+import random
+
+leds = [(255,255,255)]*360 #color white for every pixel
+
+client = opc.Client('localhost:7890')
+client.put_pixels(leds)
+client.put_pixels(leds)
+
+
+
+
