84eb874d48565fe38c760d0bf8f5b5791cd0538f
Nuno-Ferreira <nc766@live.mdx.ac.uk>
2022-01-11 16:08:24 +0000

    added another animation and reverse

--- basic animations week12.py
+++ basic animations week12.py
@@ -8,9 +8,16 @@ client = opc.Client('localhost:7890')
 client.put_pixels(leds)
 client.put_pixels(leds)
 
-for led in range(len(leds)):
-    leds[led] = (255,0,0)
+for led in range(len(leds)): #picks outs the leds
+    leds[led] = (255,0,0)  #making it red
     time.sleep(1)
     client.put_pixels(leds)
+
+led = 0 #to reverse it change to 59
+while led<60: #to reverse chaneg it to 'led>=0'
+    leds[led] = (0,255,0)
+    time.sleep(1)
+    client.put_pixels(leds)
+    led = led + 1
 
 
