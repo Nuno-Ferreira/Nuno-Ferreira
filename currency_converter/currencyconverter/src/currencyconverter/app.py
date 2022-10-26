6c89620de08707b603b12eaef391a8f29b5bb150
Nuno-Ferreira <nc766@live.mdx.ac.uk>
2022-10-26 13:28:54 +0100

    started building the final app

--- currency_converter/currencyconverter/src/currencyconverter/app.py
+++ currency_converter/currencyconverter/src/currencyconverter/app.py
@@ -4,7 +4,14 @@ My first application that converts any currency in seconds
 import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
-import currency_converter
+import android
+from android.util import Log
+from android.widget import LinearLayout
+from android.widget import Button
+from android.widget import TextView
+from android.view import Gravity
+import android.view
+from currency_converter import currency_converter
 
 
 class ButtonClick(implements=android.view.View[OnClickListener]):
@@ -17,19 +24,46 @@ class ButtonClick(implements=android.view.View[OnClickListener]):
         self.callback(*self.args, **self.kwargs)
 
 
+class CurrencyConverter:
+    def __init__(self):
+        self.activity = android.PythonActivity.setListener(self)
+        self.buttons = []
+        
+
+    def onCreate(self):
+        
+        self.convert_button = Button(self._activity)
+        self.convert_button.setText('Convert')
+        self.convert_button.setOnClickListener(ButtonClick(self.convert_currency)) #this will make the connection for the app to convert
+        vlayout.addView(self.convert_button)
+
+        footer = TextView(self._activity) #this will set the footer and the self_activity will communicate with pythonactivity 
+        footer = setText('Powered by Python')
+        footer.setGravity(Gravity.CENTER) #this will position the text
+        vlayout.addView(footer) #still need to find out what it does but maybe it adds it to the screen
+
+    def convert_currency(self):
+        self.activity
+        self.message = None
+        self.runScript()
+
+    def runScript(self):
+        
+
+"""
 class CurrencyConverter(toga.App):
     def __init__(self):
         self.buttons = []
         
 
     def startup(self):
-        """
+        
         Construct and show the Toga application.
 
         Usually, you would add your application to a main content box.
         We then create a main window (with a name matching the app), and
         show the main window.
-        """
+        
         main_box = toga.Box(style=Pack(direction=COLUMN))
 
         name_label = toga.Label(
@@ -60,7 +94,7 @@ class CurrencyConverter(toga.App):
 
     def say_hello(self, widget):
         print("Hello,", self.name_input.value)
-
+"""
 
 
 def main():
